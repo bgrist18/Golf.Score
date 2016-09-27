fd4a34484b635ff83ec627cd4f624ad45ef50419
bgrist18 <bgrist309@gmail.com>
2016-09-26 21:21:33 -0400

    excersize 4 sorting names

--- src/Person.java
+++ src/Person.java
@@ -1,62 +1,70 @@
+import java.util.Comparator;
+
 /**
  * Created by brett on 9/12/16.
  */
-public class Person {
+public class Person implements Comparable{
     String name;
     int age;
     boolean isAlive;
+    double balance;
     static int minimumAge = 18;
 
+    Person() {
 
+    }
 
-    Person(String newName, int newAge, boolean newIsAlive) {
+    Person(String newName, int newAge, boolean newIsAlive){
         setName(newName);
         setAge(newAge);
         setIsAlive(newIsAlive);
+
     }
 
-    public boolean isAlive() {
-        return isAlive;
-    }
-
-    public int getMinimumAge() {
-        return minimumAge;
-    }
-
-    String getName() {
+    public String getName() {
         return name;
     }
 
-    void setName(String newName) {
-        if (isValidName(newName)) {
-            name = newName;
-        }
+    public void setName(String name) {
+        this.name = name;
     }
 
-    int getAge() {
+    public int getAge() {
         return age;
     }
 
-    void setAge(int newAge) {
-        if (newAge >= minimumAge) {
-            age = newAge;
-        }
+    public void setAge(int age) {
+        this.age = age;
     }
 
-    boolean getIsAlive() {
+    public boolean getIsAlive() {
         return isAlive;
     }
 
-    void setIsAlive(boolean newIsAlive) {
-        if (age > 100) {
-            isAlive = false;
-        }
-        else {
-            isAlive = newIsAlive;
-        }
+    public void setIsAlive(boolean alive) {
+        isAlive = alive;
     }
 
-    static boolean isValidName(String name) {
-        return name.contains(" ");
+    public double getBalance() {
+        return balance;
+    }
+
+    public void setBalance(double balance) {
+        this.balance = balance;
+    }
+
+    @Override
+    public String toString() {
+        return "Person{" +
+                "name='" + name + '\'' +
+                ", age=" + age +
+                ", isAlive=" + isAlive +
+                ", balance=" + balance +
+                '}';
+    }
+    @Override
+    public int compareTo(Object o) {
+        Person p = (Person) o;
+        return this.name.compareTo(p.name);
     }
 }
